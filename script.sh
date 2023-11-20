9dcf0d360eddeb61dd191db17cf850a64aa0da4f
Guillermo Soanes <guillesoanes30@gmail.com>
2023-11-20 17:36:41 -0300

    Se han modificado  lineas

--- script.sh
+++ script.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-lineas_modificadas=$(git diff --stat | grep "changed" | awk '{total+=$1} END{print total}')
+git add .
 
-if [ $lineas_modificadas -gt 0 ]; then
-git commit -m "Se han modificado $lineas_modificadas lineas"
+lineas_modificadas=$(git diff --numstat | awk  '{ total += $1 + $2 } END{print total}')
+
+#if echo $lineas_modificadas | grep -q ^[0-9]\+$; then
+#   if [$lineas_modificadas -gt 0] 2>/dev/null; then
+     git commit -m "Se han modificado $lineas_modificadas lineas"
+     git push
+     echo "Cantidad de lineas modificadas: $lineas_modificadas" > README.md
+     git add README.md
+     git commit -m "Actualizacion de README.md con la cantidad de lineas modificadas" 
+     git push
+#   else
+#    echo "No se han realizado cambios. No se han realizado commits."
+#   fi
+#else
+#    echo "Hubo un problema al obtener la cantidad de lineas modificadas"
+#fi
